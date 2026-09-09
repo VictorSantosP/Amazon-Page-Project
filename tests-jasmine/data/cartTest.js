b438d0b7394ea56b0f26eb17277adf4f7037e08f
Victor Santos <vhdboss@gmail.com>
2026-09-08 23:19:38 -0300

    fixed the test problems

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addtoCart, cart, loadFromStorage}  from '../../data/cart.js'
+import { cart }  from '../../data/cart-class.js'
 describe('test suite: addToCart', () => {
     it('adds an existing product to the cart ', () => {
         spyOn(localStorage, 'setItem');
@@ -11,13 +11,15 @@ describe('test suite: addToCart', () => {
             }]);
 
         });
-        loadFromStorage();
+        cart.loadFromStorage();
+        console.log('APÓS loadFromStorage:', JSON.stringify(cart.cartItems));
 
-        addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart.length).toEqual(1);
+        cart.addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        console.log('APÓS loadFromStorage:', JSON.stringify(cart.cartItems));
+        expect(cart.cartItems.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart[0].quantity).toEqual(2);
+        expect(cart.cartItems[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.cartItems[0].quantity).toEqual(2);
     });
 
     it('adds a new product to the cart ', () => {
@@ -28,12 +30,12 @@ describe('test suite: addToCart', () => {
 
         });
 
-        loadFromStorage();
+        cart.loadFromStorage();
 
-        addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart.length).toEqual(1);
+        cart.addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.cartItems.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart[0].quantity).toEqual(1);
+        expect(cart.cartItems[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.cartItems[0].quantity).toEqual(1);
     });
 })
