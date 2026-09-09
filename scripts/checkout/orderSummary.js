ae12dfeb56115a0a691c6368977c7b80e6839f79
Victor Santos <vhdboss@gmail.com>
2026-09-09 12:40:09 -0300

    Tested with backend

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,7 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import  dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from './paymentSummary.js';
-
+import { loadProducts } from '../../data/products.js';
 
 export function renderOrderSummary (){
   let cartSummaryHTML = '';
