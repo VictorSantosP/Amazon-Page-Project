db7496166e7e057f8c7ddaba73655941d515aba5
Victor Santos <vhdboss@gmail.com>
2026-09-09 14:44:25 -0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -91,6 +91,8 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
 
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -116,9 +118,14 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+
 /*
  export const products = [
   {
