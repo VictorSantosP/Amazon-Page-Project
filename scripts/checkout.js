c8b5f3be40138652713374585e4c520c928e92fc
Victor Santos <vhdboss@gmail.com>
2026-09-09 13:41:40 -0300

    Learn promises and covert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,41 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import { loadProducts  } from "../data/products.js";
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+    new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+*/
